a56c943ed9008de9f6f1f46eb1e4a02e7f9d1892
nayoung0 <nayoung0066@gmail.com>
2016-09-30 19:40:08 +0000

    권한 설정 완료

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,14 @@ class HomeController < ApplicationController
 
   def index
     if user_signed_in?
-      @posts = Post.all.order(created_at: :desc)
+      @posts = Post.all.order(created_at: :desc) # reverse로 해도 댐
       @post= Post.new
       @friends = current_user.friends
       @requested = current_user.requested_friends
       @pending = current_user.pending_friends
       @blocked = current_user.blocked_friends
       # @writer = current_user.id.find(params[:id])
-      # @user = User.find(params[:user_id])
+      #@user = User.find(params[:user_id])
       # @todo_list = current_user.todo_lists.find(params[:todo_list_id])
       # @nickname = @friends.find(params[:user_id])
     else
@@ -34,7 +34,7 @@ class HomeController < ApplicationController
   # end
   
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :user_id, :range)
   end
   
   
